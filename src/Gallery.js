ec8dda1a6f42e63f15bb40b1119808f2acd3e169
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-27 10:44:48 +0530

    Updated All and added styles

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,23 @@
 //jshint esnext:true
-import React from 'react'
+import React from "react";
 
-const Gallery = ({data}) => {
-    return (
-        <div className="row">
-        {data.map((image) =><div key={image.id}>
-        <div className="col-md-4">
-        <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} 
-       height="200" width="250" alt={image.title}/>
+const Gallery = ({ data }) => {
+  return (
+    <div className="row">
+      {data.map((image) => (
+        <div key={image.id}>
+          <div className="col-md-4">
+            <img
+              src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`}
+              height="200"
+              width="250"
+              alt={image.title}
+            />
+          </div>
         </div>
-        </div>)}
-        </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
